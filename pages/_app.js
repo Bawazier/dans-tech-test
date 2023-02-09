e3bd0f55ec9e6b0d6a2417eb34818515f167c767
ubuntu <muhammadba.wazieer@gmail.com>
2023-02-09 12:26:32 +0700

    setup login by google account with next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,20 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ChakraProvider } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   const [queryClient] = React.useState(() => new QueryClient());
   return (
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
+          <SessionProvider session={session}>
+            <Component {...pageProps} />
+          </SessionProvider>
         </Hydrate>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
